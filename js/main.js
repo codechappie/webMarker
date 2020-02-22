2efe92f433565c94b191f3def0c70735dfa55c73
codechappie <davidproyecto01@gmail.com>
2020-02-22 16:57:23 -0500

    Upload

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const btnSearchResponsive = document.getElementById("btn-search-responsive");
   const btnCloseSerchRes = document.getElementById("btn-close-search-responsive");
+  const btn_love = document.querySelectorAll(".love img");
   const searchInput = document.getElementById("searchInput");
-  
   const formSearch = document.getElementById("formSearch");
   
   btnSearchResponsive.addEventListener("click", (e)=>{
@@ -16,5 +16,22 @@ document.addEventListener('DOMContentLoaded', function() {
     formSearch.classList.remove("show-searcher");
     searchInput.value='';
   });
-  
+
+  for (const love of btn_love){
+    love.addEventListener("click", (e)=>{
+      e.preventDefault();
+      let active = love.getAttribute("src").split("/").reverse();
+      active = active[0];
+      if(active == 'love.png'){
+        love.setAttribute("src","images/love-active.png");
+        love.setAttribute("alt","true");
+        love.classList.add("loveTransition");
+      }else{
+        love.setAttribute("alt","false");
+        love.setAttribute("src","images/love.png");
+        love.classList.remove("loveTransition");
+      }
+      
+    });
+  }
 });
