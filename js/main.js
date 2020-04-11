17dc7266c1cb596f70128af7e001bf26ea39fe79
codechappie <davidproyecto01@gmail.com>
2020-04-11 15:26:14 -0500

    upgrade

--- js/main.js
+++ js/main.js
@@ -1,37 +1,55 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   const btnSearchResponsive = document.getElementById("btn-search-responsive");
   const btnCloseSerchRes = document.getElementById("btn-close-search-responsive");
   const btn_love = document.querySelectorAll(".love img");
   const searchInput = document.getElementById("searchInput");
   const formSearch = document.getElementById("formSearch");
-  
-  btnSearchResponsive.addEventListener("click", (e)=>{
+  const addButton = document.getElementById("btn-add-new");
+  const closeModal = document.getElementById("closeModal");
+  const cancelModal = document.getElementById("cancelModal");
+  const modal = document.getElementById("modal-add-new-marker");
+
+  addButton.addEventListener("click", () => {
+    modal.style.visibility = "visible";
+  });
+  closeModal.addEventListener("click", () => {
+    modal.style.visibility = "hidden";
+  });
+  cancelModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.visibility = "hidden";
+  });
+  closeModal.addEventListener("click", (e) => {
+    modal.style.visibility = "hidden";
+  });
+  btnSearchResponsive.addEventListener("click", (e) => {
     e.preventDefault();
     formSearch.classList.add("show-searcher");
     searchInput.focus();
   });
-  btnCloseSerchRes.addEventListener("click", (e)=>{
+
+  btnCloseSerchRes.addEventListener("click", (e) => {
     e.preventDefault();
     formSearch.classList.remove("show-searcher");
-    searchInput.value='';
+    searchInput.value = '';
   });
 
-  for (const love of btn_love){
-    love.addEventListener("click", (e)=>{
+  for (const love of btn_love) {
+    love.addEventListener("click", (e) => {
       e.preventDefault();
       let active = love.getAttribute("src").split("/").reverse();
       active = active[0];
-      if(active == 'love.png'){
-        love.setAttribute("src","images/love-active.png");
-        love.setAttribute("alt","true");
+      if (active == 'love.png') {
+        love.setAttribute("src", "images/love-active.png");
+        love.setAttribute("alt", "true");
         love.classList.add("loveTransition");
-      }else{
-        love.setAttribute("alt","false");
-        love.setAttribute("src","images/love.png");
+      } else {
+        love.setAttribute("alt", "false");
+        love.setAttribute("src", "images/love.png");
         love.classList.remove("loveTransition");
       }
-      
+
     });
   }
 });
